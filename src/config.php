76b36436e533509871b4d6a4f2d877dee06227f0
BlankSpot08 <arvinchu31@gmail.com>
2022-12-04 03:20:30 +0800

    oh yeah

--- src/config.php
+++ src/config.php
@@ -145,7 +145,7 @@ $CONFIG = [
 
     // General
     "UNFORMAT_YEAR" => 50, // Unformat year
-    "RANDOM_KEY" => 'jTbMn2PxPmxwtnvf', // Random key for encryption
+    "RANDOM_KEY" => 'uSFThsg4Fd0SHG66', // Random key for encryption
     "ENCRYPTION_KEY" => '', // Encryption key for data protection
     "PROJECT_STYLESHEET_FILENAME" => "css/efga_expense_system.css", // Project stylesheet file name
     "USE_COMPRESSED_STYLESHEET" => true, // Compressed stylesheet
