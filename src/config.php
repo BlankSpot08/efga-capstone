b1f1f1c115a87902741f471a6264610add4d1160
BlankSpot08 <arvinchu31@gmail.com>
2022-12-04 04:56:25 +0800

    ksdfja

--- src/config.php
+++ src/config.php
@@ -145,7 +145,7 @@ $CONFIG = [
 
     // General
     "UNFORMAT_YEAR" => 50, // Unformat year
-    "RANDOM_KEY" => 'eZG2AtkogdDm3UMY', // Random key for encryption
+    "RANDOM_KEY" => 'H8edqpMSclorY9ii', // Random key for encryption
     "ENCRYPTION_KEY" => '', // Encryption key for data protection
     "PROJECT_STYLESHEET_FILENAME" => "css/efga_expense_system.css", // Project stylesheet file name
     "USE_COMPRESSED_STYLESHEET" => true, // Compressed stylesheet
